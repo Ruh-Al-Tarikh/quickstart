4bed374ed80ff6cb5db2fc63feda84646cd2805f
Rizwan <azrizwanm@gmail.com>
2026-02-08 23:03:06 +0530

    feat: improve getting started and logging examples

--- 01_getting_started.py
+++ 01_getting_started.py
@@ -1,15 +1,19 @@
-from prefect import flow, task
 import random
+
+from prefect import flow, task
+
 
 @task
 def get_customer_ids() -> list[str]:
     # Fetch customer IDs from a database or API
     return [f"customer{n}" for n in random.choices(range(100), k=50)]
 
+
 @task
 def process_customer(customer_id: str) -> str:
     # Process a single customer
     return f"Processed {customer_id}"
+
 
 @flow
 def main() -> list[str]:
--- 02_logging.py
+++ 02_logging.py
@@ -1,12 +1,14 @@
+import random
+
 from prefect import flow, task
 from prefect.logging import get_run_logger
-import random
 
 
 @task
 def get_customer_ids() -> list[str]:
     # Fetch customer IDs from a database or API
     return [f"customer{n}" for n in random.choices(range(100), k=50)]
+
 
 @task
 def process_customer(customer_id: str) -> str:
@@ -15,6 +17,7 @@ def process_customer(customer_id: str) -> str:
     for _ in range(50):
         logger.info(f"Processing customer {customer_id}")
     return f"Processed {customer_id}"
+
 
 @flow
 def main() -> list[str]:
